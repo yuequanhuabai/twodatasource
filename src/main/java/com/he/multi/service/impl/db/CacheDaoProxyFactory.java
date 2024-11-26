dd795ac9a8f5e880153ca686139da5df87a63c50
baiyingwaming <baiyingwaming@foxmail.com>
2024-11-26 20:26:51 +0800

    代理对象

--- src/main/java/com/he/multi/service/impl/db/CacheDaoProxyFactory.java
+++ src/main/java/com/he/multi/service/impl/db/CacheDaoProxyFactory.java
@@ -3,20 +3,20 @@ package com.he.multi.service.impl.db;
 import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.FactoryBean;
 
+import java.lang.reflect.Proxy;
+
 public class CacheDaoProxyFactory implements FactoryBean {
 
     private Class<T> daoInterface;
-
 
     public CacheDaoProxyFactory(Class<T> daoInterface) {
         this.daoInterface = daoInterface;
     }
 
-
-
     @Override
     public Object getObject() throws Exception {
-        return null;
+        CacheDaoProxyHandler<Object> tCacheDaoProxyHandler = new CacheDaoProxyHandler<>();
+        return Proxy.newProxyInstance(this.daoInterface.getClassLoader(),new Class[]{daoInterface}, tCacheDaoProxyHandler);
     }
 
     @Override
