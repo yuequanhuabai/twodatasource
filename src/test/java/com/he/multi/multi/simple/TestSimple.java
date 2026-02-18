daa0faf3b5770f51d97f653cff060c178f951ed9
baiyingwaming <baiyingwaming@foxmail.com>
2026-02-18 22:22:56 +0800

    20260218

--- src/test/java/com/he/multi/multi/simple/TestSimple.java
+++ src/test/java/com/he/multi/multi/simple/TestSimple.java
@@ -6,7 +6,7 @@ public class TestSimple {
     public static int a = 0;
 
     // 题目: 启动10个线程，每个线程执行一千次自增操作,最终的结果是10000;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Thread[] threads = new Thread[10];
 
@@ -19,6 +19,9 @@ public class TestSimple {
                     }
             );
             threads[i].start();
+        }
+        for (int i = 0; i < 10; i++) {
+            threads[i].join();
         }
 
         System.out.println("a:" + a);
