ece78f17b1f6996684b659bd71526087782a2d11
baiyingwaming <baiyingwaming@foxmail.com>
2026-02-18 22:34:43 +0800

    20260218

--- src/test/java/com/he/multi/multi/simple/TestSimple.java
+++ src/test/java/com/he/multi/multi/simple/TestSimple.java
@@ -3,7 +3,7 @@ package com.he.multi.multi.simple;
 import static sun.misc.PostVMInitHook.run;
 
 public class TestSimple {
-    public static int a = 0;
+    public static  int a = 0;
 
     // 题目: 启动10个线程，每个线程执行一千次自增操作,最终的结果是10000;
     public static void main(String[] args) throws InterruptedException {
@@ -14,7 +14,9 @@ public class TestSimple {
             threads[i] = new Thread(
                     () -> {
                         for (int j = 0; j < 1000; j++) {
-                            a++;
+                            synchronized (Object.class) {
+                                a++;
+                            }
                         }
                     }
             );
