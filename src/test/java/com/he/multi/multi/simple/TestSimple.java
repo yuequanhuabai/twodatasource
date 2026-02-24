64a6b710afee884f02558ce50d796c889caa384b
yuequanhuabai <yuequanhuabai@qq.com>
2026-02-25 06:29:11 +0800

    20260225

--- src/test/java/com/he/multi/multi/simple/TestSimple.java
+++ src/test/java/com/he/multi/multi/simple/TestSimple.java
@@ -2,7 +2,58 @@ package com.he.multi.multi.simple;
 
 
 public class TestSimple {
-    public static  volatile int a = 0;
+    public static volatile int a = 0;
+
+
+    // 题目: 启动10个线程，每个线程执行一千次自增操作,最终的结果是10000;
+//    public static void main(String[] args) throws InterruptedException {
+//
+//        Thread[] threads = new Thread[10];
+//
+//        for (int i = 0; i < 10; i++) {
+//            threads[i] = new Thread(
+//                    () -> {
+//                        for (int j = 0; j < 1000; j++) {
+//                            synchronized (Object.class) {
+//                                a++;
+//                            }
+//                        }
+//                    }
+//            );
+//            threads[i].start();
+//        }
+//        // 主线程一次加入到thread[i]中去; 直道最后一个thread[9]执行完毕;
+//        for (int i = 0; i < 10; i++) {
+//            threads[i].join();
+//        }
+//
+//        System.out.println("a:" + a);
+//    }
+
+
+// 题目: 启动10个线程，每个线程执行一千次自增操作,最终的结果是10000;
+//   public static AtomicInteger a = new AtomicInteger(0);
+//    public static void main(String[] args) throws InterruptedException {
+//
+//        Thread[] threads = new Thread[10];
+//
+//        for (int i = 0; i < 10; i++) {
+//            threads[i] = new Thread(
+//                    () -> {
+//                        for (int j = 0; j < 1000; j++) {
+//                            a.addAndGet(1);
+//                        }
+//                    }
+//            );
+//            threads[i].start();
+//        }
+//        // 主线程一次加入到thread[i]中去; 直道最后一个thread[9]执行完毕;
+//        for (int i = 0; i < 10; i++) {
+//            threads[i].join();
+//        }
+//
+//        System.out.println("a:" + a);
+//    }
 
     // 题目: 启动10个线程，每个线程执行一千次自增操作,最终的结果是10000;
     public static void main(String[] args) throws InterruptedException {
@@ -13,7 +64,7 @@ public class TestSimple {
             threads[i] = new Thread(
                     () -> {
                         for (int j = 0; j < 1000; j++) {
-                                a++;
+                            a++;
                         }
                     }
             );
@@ -26,4 +77,6 @@ public class TestSimple {
 
         System.out.println("a:" + a);
     }
+
+
 }
