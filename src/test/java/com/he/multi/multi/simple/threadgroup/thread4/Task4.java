c5e90fa7ee258b077820db152a58ef0558912aff
yuequanhuabai <yuequanhuabai@qq.com>
2026-03-16 22:04:00 +0800

    20260316-synchronied鎖對象問題

---
+++ src/test/java/com/he/multi/multi/simple/threadgroup/thread4/Task4.java
@@ -0,0 +1,34 @@
+package com.he.multi.multi.simple.threadgroup.thread4;
+
+public class Task4 extends Thread {
+
+    private int[] b;
+    private  Object c;
+
+    public int[] increment() {
+        synchronized (c) {
+            b[0]++;
+        }
+        return b;
+    }
+
+    public int[] getB() {
+        synchronized (c) {
+            return b;
+        }
+
+    }
+
+    public Task4(int[] b, Object c) {
+        this.b = b;
+        this.c = c;
+    }
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            b[0]++;
+        }
+    }
+}
---
+++ src/test/java/com/he/multi/multi/simple/threadgroup/thread4/Test4.java
@@ -0,0 +1,23 @@
+package com.he.multi.multi.simple.threadgroup.thread4;
+
+public class Test4 {
+
+    static int[] a = {0};
+    private static final Object lock = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            threads[i] = new Task4(a, lock);
+        }
+        for (int i = 0; i < 10; i++) {
+            threads[i].start();
+        }
+        for (int i = 0; i < 10; i++) {
+            threads[i].join();
+        }
+
+        System.out.println("a[0]=" + a[0]);
+
+    }
+}
