812eb62f32b67f932cfe85a5f3ef75a94d2c8c12
baiyingwaming <baiyingwaming@foxmail.com>
2026-03-17 11:37:55 +0800

    sum->initValue 精度丢失

--- src/test/java/com/he/multi/multi/simple/threadgroup/thread8/Calcutor8.java
+++ src/test/java/com/he/multi/multi/simple/threadgroup/thread8/Calcutor8.java
@@ -10,6 +10,9 @@ public class Calcutor8 {
     }
 
     public long getCount() {
+        // 为什么不用initValue而是sum，因为initValue的底层是sum，而且还会把long强转位int;会有精度丢失的风险;
+//        return count.intValue();
         return count.sum();
+
     }
 }
