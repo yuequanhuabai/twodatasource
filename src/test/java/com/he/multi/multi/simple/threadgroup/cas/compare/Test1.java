7e719277bd33aa6c1d17417acb10cfb981c8b1f0
yuequanhuabai <yuequanhuabai@qq.com>
2026-03-24 23:32:05 +0800

    compare

---
+++ src/test/java/com/he/multi/multi/simple/threadgroup/cas/compare/Test1.java
@@ -0,0 +1,16 @@
+package com.he.multi.multi.simple.threadgroup.cas.compare;
+
+/**
+ ***LongAdder vs AtomicLong 性能对比**
+ *
+ * 编写一个性能测试程序，对比 `AtomicLong` 和 `LongAdder` 在高并发场景下的性能差异。
+ *
+ * 要求：
+ * 1. 分别使用 AtomicLong 和 LongAdder
+ * 2. 启动100个线程，每个线程执行100万次累加
+ * 3. 记录两种方式的耗时
+ * 4. 分析性能差异的原因
+ *
+ */
+public class Test1 {
+}
